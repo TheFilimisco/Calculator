b1c4f867069e71a24b54e5113776b99f3e9c5ae8
TheFilimisco <54910978l@iespoblenou.org>
2024-11-26 22:04:58 +0100

    Add divide method

--- src/Calculator.java
+++ src/Calculator.java
@@ -10,10 +10,19 @@ public class Calculator {
         return a * b;
     }
 
+    public int divide(int a, int b) {
+        if (a == 0 || b == 0) {
+            return 0;
+        }
+        return a/b;
+    }
+
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
         System.out.println("Addition: " + calculator.add(10, 5));
         System.out.println("Subtraction: " + calculator.subtract(10, 5));
         System.out.println("Multiplication: " + calculator.multiply(10, 5));
+        System.out.println("Divisio: " + calculator.divide(10, 5));
+        System.out.println("Divisio: " + calculator.divide(0, 5));
     }
 }
